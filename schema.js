c9e5c036ab8f7295e086007c84c767f611cb748e
marcos8896 <elyomarcos@gmail.com>
2018-04-24 22:22:39 -0500

    Building a GraphQL Server [Part 4] - JSON Server & Axios

--- schema.js
+++ schema.js
@@ -3,6 +3,7 @@ const {
   GraphQLSchema, GraphQLList, GraphQLNonNull,
 } = require('graphql');
 
+const axios = require('axios');
 
 //Customer type.
 const CustomerType = new GraphQLObjectType({
@@ -16,14 +17,6 @@ const CustomerType = new GraphQLObjectType({
 });
 
 
-//Hardcoded data.
-const customers = [
-  { id: '1', name: 'Juan Doe', email: 'juanito@gmail.com', age: 42 },
-  { id: '2', name: 'Estefano Esmeraldez', email: 'estef@gmail.com', age: 32 },
-  { id: '3', name: 'Sara Yiyi', email: 'sara@gmail.com', age: 22 },
-];
-
-
 //Root query.
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -34,13 +27,15 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLString }
       },
       resolve(parentValue, args) {
-        return customers.find( customer => customer.id == args.id);
+        return axios.get(`http://localhost:3000/customers/${args.id}`)
+          .then(res => res.data)        
       }
     },
     customers: {
       type: new GraphQLList(CustomerType),
       resolve(parentValue, args){
-        return customers;
+        return axios.get(`http://localhost:3000/customers/`)
+          .then(res => res.data)
       }
     }
   }
